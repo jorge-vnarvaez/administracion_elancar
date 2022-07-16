710d9da61a00595b31cffb79610697151f7b3d45
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-07-15 23:05:12 -0400

    h-screen to home agregado

--- pages/home.vue
+++ pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col justify-center align-center">
+    <div class="flex flex-col justify-center align-center h-screen">
         <span class="block text-5xl font-bold">¿Qué tipo de producto busca el cliente?</span>
         <span class="block text-xl mt-4">Seleccione un elemento</span>
         <macro-categorias />
