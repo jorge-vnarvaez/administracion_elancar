3aec74d8bd640add653cc84023cdd8e42c62b7c0
1TzYersonzT1 <yersonherrera14@gmail.com>
2022-09-27 04:40:00 -0300

    add form compra y despacho v1

---
+++ components/reusable/OpcionDespacho.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tw-flex align-center">
+    <!-- RETIRO EN TIENDA-->
+    <div v-if="tipo == 'retiro_en_tienda'"><IconoRetiroTienda /></div>
+    <!-- RETIRO EN TIENDA-->
+
+    <!-- DESPACHO A DOMICILIO-->
+    <div v-if="tipo != 'retiro_en_tienda'"><IconoDespachoDomicilio /></div>
+    <!-- DESPACHO A DOMICILIO-->
+
+    <!--RADIO BUTTONS-->
+    <div>
+      <v-radio :value="tipo">
+        <template v-slot:label>
+          <div>
+            <strong class="tw-text-neutral-900">{{ titulo }}</strong>
+          </div>
+        </template>
+      </v-radio>
+        <span class="tw-block tw-text-sky-900 tw-w-40 tw-ml-8">{{ descripcion }}</span>
+    </div>
+    <!--RADIO BUTTONS-->
+  </div>
+</template>
+
+<script>
+import IconoRetiroTienda from "@/components/iconos/IconoRetiroTienda.vue";
+import IconoDespachoDomicilio from "@/components/iconos/IconoDespachoDomicilio.vue";
+export default {
+  components: {
+    IconoRetiroTienda,
+    IconoDespachoDomicilio,
+  },
+  props: {
+    tipo: {
+      type: String,
+      required: true,
+    },
+    titulo: {
+      type: String,
+      required: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
